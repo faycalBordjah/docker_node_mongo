c61de1b73a7e511c18fc55d5794c8f70473eb5da
Fayçal BORDJAH <fbordjah@linagora.com>
2019-12-04 11:19:08 +0100

    Dockerize the application

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const hostname = '0.0.0.0';
+const port = 8080;
+const User = require('./api/models/userModel');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://mongo:27017/ipssi2019', {useUnifiedTopology: true, useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const routes = require('./api/routes/userRoutes');
+routes(app);
+
+app.listen(port, hostname);
